737a3f20a855e390685306528fc648afe5391557
Jorge Espinoza <jespinoza@angelfilms.cl>
2022-11-09 11:45:54 -0300

    main

--- main.js
+++ main.js
@@ -8,23 +8,11 @@ import {
     from "./firebase.js"
 
 window.addEventListener("DOMContentLoaded", () => {
-    //     console.log("App Cargada")
 
     init();
     window.setCodigoActividad = setCodigoActividad;
     window.setCodigoDiapositiva = setCodigoDiapositiva;
-    window.setEstadoSolucionario = setEstadoSolucionario;
     window.setIntentos = setIntentos;
-
-    //     // setCodigoActividad("NC34U5A01")
-    //     setCodigoDiapositiva("NC34U5A01", "NC34U5A01D01");
-
-
-    //     document.getElementById("btn_intentos").addEventListener("click", () => {
-    //         setIntentos("NC34U5A01", "NC34U5A01D01");
-    //     })
-    //     document.getElementById("btn_solucionario").addEventListener("click", () => {
-    //         setEstadoSolucionario("NC34U5A01", "NC34U5A01D01")
-    //     })
+    window.setEstadoSolucionario = setEstadoSolucionario;
 
 })
